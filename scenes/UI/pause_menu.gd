d86ece92728fe58e9ff80266bf3122f283969c6a
Alendart <111238215+Alendart@users.noreply.github.com>
2023-08-19 23:06:24 +0200

    Added pause menu and pause script

---
+++ scenes/UI/pause_menu.gd
@@ -0,0 +1,55 @@
+extends CanvasLayer
+
+@export var option_scene:PackedScene
+
+@onready var panel_container = $MarginContainer/PanelContainer
+var is_closing:bool = false
+
+func _ready():
+	get_tree().paused = true
+	panel_container.pivot_offset = panel_container.size / 2
+	%ResumeButton.pressed.connect(on_resume_pressed)
+	%OptionButton.pressed.connect(on_option_pressed)
+	%QuitButton.pressed.connect(on_quit_pressed)
+	
+	
+	$AnimationPlayer.play("default")
+	
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+
+func close_menu():
+	if !is_closing:
+		is_closing = true
+		$AnimationPlayer.play_backwards("default")
+	
+		var tween = create_tween()
+		tween.tween_property(panel_container, "scale", Vector2.ONE, 0)
+		tween.tween_property(panel_container, "scale", Vector2.ZERO, 0.3)\
+		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	
+		await tween.finished
+		get_tree().paused = false
+		queue_free()
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("Pause"):
+		close_menu()
+		get_tree().root.set_input_as_handled()
+
+func on_resume_pressed():
+	close_menu()
+
+func on_option_pressed():
+	var options = option_scene.instantiate()
+	add_child(options)
+	options.options_quited.connect(on_options_quited.bind(options))
+
+func on_quit_pressed():
+	get_tree().change_scene_to_file("res://scenes/UI/main_menu.tscn")
+
+func on_options_quited(options:Node):
+	options.queue_free()
