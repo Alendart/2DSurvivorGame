c400b783e0c0ac3ee3e8f02958d886250a76e938
Alendart <111238215+Alendart@users.noreply.github.com>
2023-08-19 23:07:47 +0200

    added Pause menu handling from main game

--- scenes/Main/main.gd
+++ scenes/Main/main.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 @export var end_screen:PackedScene
 
+var pause_menu = preload("res://scenes/UI/pause_menu.tscn")
+
 func _ready():
 	%Player.health_component.died.connect(on_player_died)
 	$ExpierienceManager.level_up.emit(1)
 	
+
+func _unhandled_input(event):
+	if event.is_action_pressed("Pause"):
+		add_child(pause_menu.instantiate())
+		get_tree().root.set_input_as_handled()
 
 func on_player_died():
 	var end_screen_instance = end_screen.instantiate()
