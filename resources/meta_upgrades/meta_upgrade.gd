20f30d254c2fe62bd79081d9d5d1aa39f57a89ec
Alendart <111238215+Alendart@users.noreply.github.com>
2023-08-20 13:42:02 +0200

    Created scene to control meta progress via game and added resource to store data there

---
+++ resources/meta_upgrades/meta_upgrade.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+@export var id:String
+@export var max_lvl: int = 1
+@export var title: String
+@export_multiline var descripton: String
---
+++ scenes/autoload/meta_progression.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var meta_upgrades: Dictionary = {
+	"win_count":0,
+	"loss_count":0,
+	"meta_upgrade_currency":0,
+	"meta_upgrades":{}
+	}
+
+func _ready():
+	
