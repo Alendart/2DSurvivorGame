7e05fe0168ae1a558402e38a8809cea18909f109
Alendart <111238215+Alendart@users.noreply.github.com>
2023-08-20 13:38:20 +0200

    Added Money drop component

---
+++ scenes/components/money_drop_component.gd
@@ -0,0 +1,25 @@
+extends Node
+class_name MoneyDropComponent
+
+@export_range(0,1) var drop_percent: float = 0.5 
+@export var health_component: Node
+@export var money: PackedScene
+
+
+func _ready():
+	(health_component as HealthComponent).died.connect(on_died)
+	
+
+func on_died():
+	if money == null or not owner is Node2D:
+		return
+	
+	var spawned = randf_range(0,1)
+	if spawned <= drop_percent:
+		var spawn_position = (owner as Node2D).global_position + Vector2(randf_range(-10,10),randf_range(-10,10))
+		var money_instance = money.instantiate()
+		var entites_layer = get_tree().get_first_node_in_group("entities_layer")
+		entites_layer.add_child(money_instance)
+		money_instance.global_position = spawn_position
+	
+
