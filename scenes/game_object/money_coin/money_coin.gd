d9ba5cbd06696f08b51d049218985017df179fa6
Alendart <111238215+Alendart@users.noreply.github.com>
2023-08-20 13:37:32 +0200

    Added money coin scene and script

---
+++ scenes/game_object/money_coin/money_coin.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
+@onready var sprite = $Sprite2D
+
+
+
+func tween_collect(percent: float, start_position: Vector2):
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+	
+	global_position = start_position.lerp(player.global_position, percent)
+	var direction_from_start = player.global_position - start_position
+	
+	var target_rotation = direction_from_start.angle() + deg_to_rad(90)
+	rotation = lerp_angle(rotation,target_rotation, 1 - exp(-2 * get_process_delta_time()))
+
+func collect():
+	GameEvents.emit_money_coin_collected(1)
+	queue_free()
+
+func disable_collision():
+	collision_shape_2d.disabled = true
+
+func _on_area_2d_area_entered(_area):
+	Callable(disable_collision).call_deferred()
+	
+	var tween = create_tween()
+	tween.set_parallel()
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 0.5)\
+	.set_ease(Tween.EASE_IN)\
+	.set_trans(Tween.TRANS_BACK)
+	tween.tween_property(sprite,"scale",Vector2.ZERO,0.05).set_delay(0.45)
+	tween.chain()
+	tween.tween_callback(collect)
+	
+	$RandomStreamPlayer2DComponent.play_random()
+	
+
+
