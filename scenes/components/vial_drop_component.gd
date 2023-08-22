a7a3c31480eadcfaeca58ceae48af1a7253f25e1
Alendart <111238215+Alendart@users.noreply.github.com>
2023-08-22 23:02:17 +0200

    Implemented experience upgrade to affect dropVialComponent

--- scenes/components/vial_drop_component.gd
+++ scenes/components/vial_drop_component.gd
@@ -14,8 +14,11 @@ func on_died():
 	if vial == null or not owner is Node2D:
 		return
 	
+	var current_modifier = MetaProgression.check_upgrade_lvl("experience_gain")
+	var adjusted_percent = drop_percent + (0.1 * current_modifier)
+	
 	var spawned = randf_range(0,1)
-	if spawned <= drop_percent:
+	if spawned <= adjusted_percent:
 		var spawn_position = (owner as Node2D).global_position
 		var vial_instance = vial.instantiate()
 		var entites_layer = get_tree().get_first_node_in_group("entities_layer")
