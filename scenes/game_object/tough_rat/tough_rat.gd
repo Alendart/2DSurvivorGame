fa17dd8bf30576d37fdda22031994d32b8b304cd
Alendart <111238215+Alendart@users.noreply.github.com>
2023-08-24 23:38:26 +0200

    Created Tough rat enemy - variant of basic rat

---
+++ scenes/game_object/tough_rat/tough_rat.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 45
+
+@onready var velocity_component:VelocityComponent = $VelocityComponent
+
+func _ready():
+	$HurtBoxComponent.hit.connect(on_hit)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
+#	var direction = get_direction_to_player()
+#	velocity = direction * MAX_SPEED
+#	move_and_slide()
+#
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		$Visuals.scale = Vector2(move_sign,1)
+
+
+func get_direction_to_player():
+	var player = get_tree().get_first_node_in_group("player")
+	if player != null:
+		return (player.global_position - global_position).normalized()
+	return Vector2.ZERO
+
+func on_hit():
+	$RandomHitStreamPlayer2DComponent.play_random()
