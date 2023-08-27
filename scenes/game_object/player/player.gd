519831049cb99eb564acf5f42ee5030fb7d49eb4
Alendart <111238215+Alendart@users.noreply.github.com>
2023-08-28 00:43:11 +0200

    Handled meta upgrade for player

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -11,13 +11,19 @@ const MAX_HEALTH_RISE = 5
 @onready var animation_player:AnimationPlayer = $AnimationPlayer
 @onready var velocity_component = $VelocityComponent
 
+var floating_text_scene:PackedScene = preload("res://scenes/UI/floating_text.tscn")
+
 var qty_colliding_bodies = 0
 var current_speed:float
+var current_dodge = 0
 
 func _ready():
 	current_speed = velocity_component.max_speed
 	GameEvents.new_ability_added.connect(on_new_ability_added)
 	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
+	check_health_upgrade()
+	check_health_regen_upgrade()
+	check_dodge_upgrade()
 
 
 func _process(delta):
@@ -40,6 +46,20 @@ func _process(delta):
 	update_health_bar()
 	
 
+func check_health_upgrade():
+	var current_modifier = MetaProgression.check_upgrade_lvl("max_health")
+	var added_health = current_modifier * 2.5
+	health_component.rise_max_health(added_health)
+
+func check_health_regen_upgrade():
+	var current_modifier = MetaProgression.check_upgrade_lvl("health_regen")
+	var regen_rate = current_modifier * 0.5
+	health_component.health_regeneration(regen_rate)
+
+func check_dodge_upgrade():
+	var current_modifier = MetaProgression.check_upgrade_lvl("dodge")
+	current_dodge = 0.05 * current_modifier
+
 func get_movement_vector():	
 	var x_move = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_move = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
@@ -50,7 +70,22 @@ func update_health_bar():
 	health_bar.value = health_component.get_health_percent()
 
 func check_and_deal_damage():
-	if qty_colliding_bodies > 0 and damage_timer.is_stopped():
+	var attack_dodged:bool
+	if !damage_timer.is_stopped():
+		return
+	if current_dodge > 0:
+		var attack_roll = randf_range(0,1)
+		attack_dodged = attack_roll <= current_dodge
+	
+	if attack_dodged:
+		var floating_text = floating_text_scene.instantiate() as Node2D
+		get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+		floating_text.global_position = global_position + (Vector2.UP * 16)
+		floating_text.start("Dodged")
+		damage_timer.start()
+		return
+	
+	if qty_colliding_bodies > 0:
 		health_component.damage(1)
 		$HitRandomStreamPlayer.play_random()
 		GameEvents.emit_player_damaged()
