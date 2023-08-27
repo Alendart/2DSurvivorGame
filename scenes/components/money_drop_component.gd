76276523f204c02403cc134eb6a16d5237e8e6aa
Alendart <111238215+Alendart@users.noreply.github.com>
2023-08-28 00:44:26 +0200

    Implemented meta upgrade

--- scenes/components/money_drop_component.gd
+++ scenes/components/money_drop_component.gd
@@ -9,13 +9,17 @@ class_name MoneyDropComponent
 func _ready():
 	(health_component as HealthComponent).died.connect(on_died)
 	
+	
 
 func on_died():
 	if money == null or not owner is Node2D:
 		return
 	
+	var current_modifier = MetaProgression.check_upgrade_lvl("money_drop")
+	var adjusted_percent = drop_percent + (0.1 * current_modifier)
+	
 	var spawned = randf_range(0,1)
-	if spawned <= drop_percent:
+	if spawned <= adjusted_percent:
 		var spawn_position = (owner as Node2D).global_position + Vector2(randf_range(-10,10),randf_range(-10,10))
 		var money_instance = money.instantiate()
 		var entites_layer = get_tree().get_first_node_in_group("entities_layer")
