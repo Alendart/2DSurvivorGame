3b67d5383c7782440ee2ce72c6cec84786fcf312
Alendart <111238215+Alendart@users.noreply.github.com>
2023-09-02 01:18:34 +0200

    Prepared save manager script

--- scenes/UI/save_manager.gd
+++ scenes/UI/save_manager.gd
@@ -1,17 +1,26 @@
 extends CanvasLayer
 
-@onready var save_box = $MarginContainer/MarginContainer/VBoxContainer/SaveBox
-@onready var save_box_2 = $MarginContainer/MarginContainer/VBoxContainer/SaveBox2
-@onready var save_box_3 = $MarginContainer/MarginContainer/VBoxContainer/SaveBox3
+@export var save_box_scene: PackedScene
 
-var active_slots = 0
+@onready var v_box_container = %VBoxContainer
 
+var active_slot = 0
+var occupaied_slots: Dictionary
 
 func _ready():
-	GameEvents.save_path_changed.connect(on_save_changed)
-
-
-func on_save_changed(path: String):
-	pass
+	active_slot = SaveSlotManager.get_actual_user_number()
+	occupaied_slots = SaveSlotManager.get_occupaied_slots_info()
+	display_save_slots()
  
+func display_save_slots():
+	var index = 1
+	for i in 3:
+		var save_box_instance = save_box_scene.instantiate()
+		v_box_container.add_child(save_box_instance)
+		save_box_instance.slot_num = index
+		save_box_instance.active = index == active_slot
+		save_box_instance.occupaied = occupaied_slots[index]
+		save_box_instance.update_save_box_display()
+		index += 1
+
 
